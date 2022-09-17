629f074eb419eb8ba06d95893db4933d436a2f02
Braxton Ward <bward@grio.com>
2022-09-16 16:48:19 -0800

    add the ability to select answers to a question and check if the answer is correct

--- LearningApp/Views/TestView.swift
+++ LearningApp/Views/TestView.swift
@@ -9,6 +9,9 @@ import SwiftUI
 
 struct TestView: View {
     @EnvironmentObject var model: ContentModel
+    @State var selectedAnswerIndex: Int?
+    @State var numCorrect = 0
+    @State var isSubmitted = false
     
     var body: some View {
         if let currentQuestion = model.currentQuestion, let currentModule = model.currentModule {
@@ -16,23 +19,63 @@ struct TestView: View {
             let answers = currentQuestion.answers
             
             VStack(alignment: .leading) {
-                Text("Question \(model.currentQuestionIndex + 1) of \(questionCount)")
-                
-                CodeTextView()
-                
-                ForEach(0..<answers.count, id: \.self) { index in
-                    let answer = answers[index]
+                VStack(alignment: .leading) {
+                    Text("Question \(model.currentQuestionIndex + 1) of \(questionCount)")
                     
-                    Button(action: {}) {
-                        ZStack {
-                            RectangleCard()
-                            Text(answer)
+                    CodeTextView()
+                }
+                .padding()
+                
+                ScrollView {
+                    VStack {
+                        ForEach(0..<answers.count, id: \.self) { index in
+                            let answer = answers[index]
+                            let selectedAnswer = index == selectedAnswerIndex
+                            
+                            Button(action: { selectedAnswerIndex = index }) {
+                                ZStack {
+                                    if !isSubmitted {
+                                        RectangleCard(color: selectedAnswer ? .gray : .white )
+                                    } else {
+                                        let isCorrectAnswer = index == currentQuestion.correctIndex
+                                        
+                                        RectangleCard(
+                                            color: selectedAnswer ? isCorrectAnswer ? .green : .red
+                                                : isCorrectAnswer ? .green : .white
+                                        )
+                                    }
+                                    
+                                    Text(answer)
+                                        .bold()
+                                        .foregroundColor(.black)
+                                }
+                            }
+                            .disabled(isSubmitted)
                         }
                     }
+                    .accentColor(.black)
+                    .padding()
                 }
-                Spacer()
+                
+                Button(action: {
+                    isSubmitted = true
+                    // Check the answer
+                    if selectedAnswerIndex == currentQuestion.correctIndex {
+                        numCorrect += 1
+                    }
+                }) {
+                    ZStack {
+                        RectangleCard(color: .green)
+                        Text("Submit")
+                            .foregroundColor(.white)
+                            .bold()
+                    }
+                }
+                .padding()
+                .disabled(isSubmitted || selectedAnswerIndex == nil)
+                
+                
             }
-            .padding()
             .navigationBarTitle("\(currentModule.category) Test")
         } else {
             ProgressView()
